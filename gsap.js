9e3d37389ea61d91388fc60dadf05d0305d19487
DesignCrumble <kshitij2994@gmail.com>
2015-12-10 06:04:08 +0530

    Final Update
    Removed unnecessary file | tags | divs

--- gsap.js
+++ gsap.js
@@ -1,5 +1,5 @@
 window.onload = function(){
 
 var ball2 = $(".fa-circle");
-  TweenLite.to(ball2, 2, {marginTop:"354px", ease:Bounce.easeOut});
+  TweenLite.to(ball2, 2, {marginTop:"390px", ease:Bounce.easeOut , delay:0.8});
 }
